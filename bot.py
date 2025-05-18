7f2947909258517617615265f643d7eb38d18212
Serhiy Vorobyov <vitaliyvorobyov@Vitaliys-MacBook-Pro.local>
2025-05-18 17:56:43 +0200

    final: production-ready bot

--- bot.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHANNEL_USERNAME = "@top10trendingprojects"
-
-bot = Bot(token=BOT_TOKEN)
-bot.send_message(chat_id=CHANNEL_USERNAME, text="✅ Бот работает! Render запущен и может слать сообщения.")
