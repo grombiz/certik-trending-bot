3732ca7a5ee1740b6475e427682045defc42801b
Serhiy Vorobyov <vitaliyvorobyov@Vitaliys-MacBook-Pro.local>
2025-08-18 19:29:29 +0200

    Добавлены run-скрипты и GitHub Actions для автоматического запуска
    # Conflicts:
    #	send_daily_update.py

--- send_daily_update.py
+++ send_daily_update.py
@@ -1,7 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # send_daily_update.py
 =======
 >>>>>>> 92ee8f1 (Create send_daily_update.py)
+=======
+=======
+# send_daily_update.py
+>>>>>>> edba6f0 (Добавлен скрипт для разового запуска)
+>>>>>>> 5bd5f6a (Добавлены run-скрипты и GitHub Actions для автоматического запуска)
 from bot import send_daily_report, send_crypto_news
 
 if __name__ == "__main__":
